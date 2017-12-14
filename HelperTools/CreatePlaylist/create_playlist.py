83979c3a6c704477093f15f25fffca71618b5821
Felix Nagel <felix.nagel@sew-eurodrive.de>
2017-12-14 07:43:02 +0100

    refactoring

--- HelperTools/CreatePlaylist/create_playlist.py
+++ HelperTools/CreatePlaylist/create_playlist.py
@@ -26,18 +26,21 @@ def get_playlist_file_name(actual_dir):
     return actual_dir + ".m3u"
 
 
-dirnames = os.walk(".".decode('utf8')).next()[1]
-for dirname in dirnames:
-    playlist_name = dirname
-    print "playlistname: " + playlist_name
-    f = file(get_playlist_file_name(playlist_name), "w")
-    playlist_dir_mp3 = ".\\" + dirname + "\\*.mp3"
-    print playlist_dir_mp3
-    for music_file in sorted(glob.glob(playlist_dir_mp3), key=get_track):
-        print music_file
-        music_file_path = playlist_path_prefix + playlist_name + "/" + os.path.basename(music_file)
-        print music_file_path
-        f.writelines(music_file_path.encode('utf8') + "\n")
+def get_music_file_path(music_file, playlist_name):
+    return (playlist_path_prefix + playlist_name + "/" + os.path.basename(music_file)).encode('utf8') # linux path
 
+
+def create_playlist(dir_name):
+    playlist_name = dir_name
+    f = file(get_playlist_file_name(playlist_name), "w")
+    playlist_files_pattern = ".\\" + playlist_name + "\\*.mp3"  # windows path
+    for music_file in sorted(glob.glob(playlist_files_pattern), key=get_track): # sort by track
+        music_file_path = get_music_file_path(music_file, playlist_name)
+        f.writelines(music_file_path + "\n")
     f.close()
+
+
+dir_names = os.walk(".".decode('utf8')).next()[1]
+for dir_name in dir_names:
+    create_playlist(dir_name)
 
