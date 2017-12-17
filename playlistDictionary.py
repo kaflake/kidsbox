574552369bf0b27d2e7538c2dbe47de754951346
kaflake <kaflake@googlemail.com>
2017-12-17 19:56:33 +0100

    direkt playlist

--- playlistDictionary.py
+++ playlistDictionary.py
@@ -8,5 +8,9 @@ playlist_dictionary = {
     "0005432684": "Rolfs Liederkalender - Die Jahresuhr",
     "0005524439": "Giraffenaffen",
     "0005313519": "Giraffenaffen 2",
-    "0005585695": "Die 30 besten Kirchenlieder"
+    "0005585695": "Die 30 besten Kirchenlieder",
+    "0005469800": "Giraffenaffen 3",
+    "0005585688": "Unter Meinem Bett",
+    "0005585919": "Unter Meinem Bett 2",
+    "0005313686": "Unter Meinem Bett 3"
 }
